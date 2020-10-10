a312608eed6822e2a31771297b599c95ae08d974
batista12 <50904472+batista12@users.noreply.github.com>
2020-10-09 19:04:36 -0700

    [Manasi Singh]UC4 is created:UC4 is created

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,21 +1,20 @@
 package com.capgemini.userregistration;
-	import org.apache.logging.log4j.LogManager;
-	import org.apache.logging.log4j.Logger;
-	import java.util.Scanner;
-	public class UserRegistration {
-		private static final Logger LOG = LogManager.getLogger(UserRegistration.class);
-		private boolean validateEmail(String email) {
-			return email.matches("^[a-zA-Z0-9_]+([.+-]{1}[a-zA-Z0-9_]+)*[@]{1}[a-zA-Z0-
-					9]+[.]{1}[a-zA-Z0-9]{2,}([.]{1}[a-zA-Z]{2,})?$");
-		}
-		public static void main( String[] args ) {
-			Scanner sc = new Scanner(System.in);
-			LOG.info("Email Id: ");
-			String email = sc.nextLine();
-			UserRegistration userRegistration = new UserRegistration();
-			if(userRegistration.validateEmail(email))
-				LOG.info("Valid");
-			else
-				LOG.info("Invalid");
-		}
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.Scanner;
+public class UserRegistration {
+	private static final Logger LOG = LogManager.getLogger(UserRegistration.class);
+	private boolean validateMobileNo(String no) {
+		return no.matches("^[0-9]{2}[ ][0-9]{10}$");
 	}
+	public static void main( String[] args ) {
+		Scanner sc = new Scanner(System.in);
+		LOG.info("Mobile Number: ");
+		String no = sc.nextLine();
+		UserRegistration userRegistration = new UserRegistration();
+		if(userRegistration.validateMobileNo(no))
+			LOG.info("Valid");
+		else
+			LOG.info("Invalid");
+	}
+}
