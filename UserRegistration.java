a4037fc53b23b6fa6b6fe9bc44853190b4511b9e
batista12 <50904472+batista12@users.noreply.github.com>
2020-10-09 19:02:17 -0700

    [Manasi Singh]UC3 is created:UC3 is created

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,20 +1,21 @@
 package com.capgemini.userregistration;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import java.util.Scanner;
-public class UserRegistration {
-	private static final Logger LOG = LogManager.getLogger(UserRegistration.class);
-	private boolean validateLastName(String lastName) {
-		return lastName.matches("^[A-Z]{1}[a-z]{2,}$");
+	import org.apache.logging.log4j.LogManager;
+	import org.apache.logging.log4j.Logger;
+	import java.util.Scanner;
+	public class UserRegistration {
+		private static final Logger LOG = LogManager.getLogger(UserRegistration.class);
+		private boolean validateEmail(String email) {
+			return email.matches("^[a-zA-Z0-9_]+([.+-]{1}[a-zA-Z0-9_]+)*[@]{1}[a-zA-Z0-
+					9]+[.]{1}[a-zA-Z0-9]{2,}([.]{1}[a-zA-Z]{2,})?$");
+		}
+		public static void main( String[] args ) {
+			Scanner sc = new Scanner(System.in);
+			LOG.info("Email Id: ");
+			String email = sc.nextLine();
+			UserRegistration userRegistration = new UserRegistration();
+			if(userRegistration.validateEmail(email))
+				LOG.info("Valid");
+			else
+				LOG.info("Invalid");
+		}
 	}
-	public static void main( String[] args ) {
-		Scanner sc = new Scanner(System.in);
-		LOG.info("Last Name: ");
-		String lastName = sc.nextLine();
-		UserRegistration userRegistration = new UserRegistration();
-		if(userRegistration.validateLastName(lastName))
-			LOG.info("Valid");
-		else
-			LOG.info("Invalid");
-	}
-}
